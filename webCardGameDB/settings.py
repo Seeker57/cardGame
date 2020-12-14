3feb23802394b8854bf33dc19fb689e033c397cf
Andrew Belyaev <andrey.belyaev.2000@yandex.ru>
2020-12-15 00:23:09 +0300

    fix a views for decks

--- webCardGameDB/settings.py
+++ webCardGameDB/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.18', '127.0.0.1', '192.168.0.11']
 
 
 # Application definition
